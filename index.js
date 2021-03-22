0a730c74b242f2e3da097391a27910f7ce73f6d5
Ethan Kaseff <ethan@kaseff.com>
2021-03-22 09:53:19 -0500

    set up index pages with structure and plan

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.getElementById('quote-button').addEventListener('click', () => {
+        // Make HTTP Request
+
+        // Format correctly 
+        
+        // Put on the page 
+
+
+    })
+
+    document.getElementById('save-button').addEventListener('click', () => {
+        // Make sure I have new data  
+
+        // Get data that might already be there
+
+        // Add to older object
+
+        // push to local storage 
+    })
+
+
+
+
+
+
+
+})
